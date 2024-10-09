8b249df879b5ca5272608fb568b6973522b49365
Your Name <tylerjohnsononline@gmail.com>
2024-10-09 14:16:45 -0500

    works from the command line with user input

--- chartmaker.py
+++ chartmaker.py
@@ -8,9 +8,14 @@ import chart_maker_library
 def usable_chart_maker():
   path = "chart_maker\place_photo_to_be_charted_here\photo_to_become_chart.png"
   # picture = cv2.imread(path)
+  print("how many vertical")
+  print()
+  vertical = int(input())
+  print("how many horizontal")
+  horizontal = int(input())
   chart_maker_library.first_photo_chart(path = path,
-                                        vertical_default=20,
-                    horizontal_default=15,
+                                        vertical_default=vertical,
+                    horizontal_default=horizontal,
                     darker_than_default=100)
 
 
